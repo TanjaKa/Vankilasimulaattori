9e2289b11aa6b7892f2236e0deb1bbfd581a7da9
tiina <tiina@DESKTOP-D2K8O0N.wlan.metropolia.fi>
2016-03-01 13:43:28 +0200

    Muokattu ehkä jotain (Tanjalle näytiksi)

--- src/Vangit.java
+++ src/Vangit.java
@@ -8,17 +8,25 @@ public class Vangit {
     private Vanki vanki;
 
     public Vangit() {
-        vangitLista = new ArrayList<Vanki>();
+       vangitLista = new ArrayList<Vanki>();
+        
+        // Vanginhoitajat: Teemu Kontiolahti, Lauri Ronkainen, Maiju Mäkelä
 
-        vangitLista.add(new Vanki("Matti Mallikas", "Mies", "12.12.1956", "tappo", 10, "Pelle"));
-        vangitLista.add(new Vanki("Matti Mallikas", "Mies", "12.12.1956", "tappo", 10, "Pelle"));
-        vangitLista.add(new Vanki("Matti Mallikas", "Mies", "12.12.1956", "tappo", 10, "Pelle"));
-        vangitLista.add(new Vanki("Matti Mallikas", "Mies", "12.12.1956", "tappo", 10, "Pelle"));
-        vangitLista.add(new Vanki("Matti Mallikas", "Mies", "12.12.1956", "tappo", 10, "Pelle"));
-        vangitLista.add(new Vanki("Matti Mallikas", "Mies", "12.12.1956", "tappo", 10, "Pelle"));
-        vangitLista.add(new Vanki("Matti Mallikas", "Mies", "12.12.1956", "tappo", 10, "Pelle"));
-        vangitLista.add(new Vanki("Matti Mallikas", "Mies", "12.12.1956", "tappo", 10, "Pelle"));
+        vangitLista.add(new Vanki("Tuomas Valtimo", "Mies", "05.02.1981", "törkeä raiskaus", 2, "Teemu Kontiolahti"));
+        vangitLista.add(new Vanki("Ari Eronen", "Mies", "11.06.1951", "törkeä raiskaus", 2, "Lauri Ronkainen"));
+        vangitLista.add(new Vanki("Ville Mutanen", "Mies", "09.10.1969", "tappo", 8, "Teemu Kontiolahti"));
+        vangitLista.add(new Vanki("Lauri Paltamo", "Mies", "12.12.1956", "törkeä huumausainerikos", 1, "Lauri Ronkainen"));
+        vangitLista.add(new Vanki("Pirjo Kärkkäinen", "Nainen", "25.04.1972", "tappo", 8, "Teemu Kontiolahti"));
+        vangitLista.add(new Vanki("Petteri Volmanen", "Mies", "06.03.1993", "törkeä pahoinpitely", 1, "Maiju Mäkelä"));
+        vangitLista.add(new Vanki("Juha Matilainen", "Mies", "12.12.1956", "tappo", 8, "Teemu Kontiolahti"));
+        vangitLista.add(new Vanki("Anne Pasanen", "Nainen", "01.12.1968", "törkeä huumausainerikos", 1, "Lauri Ronkainen"));
+        vangitLista.add(new Vanki("Kata Lankinen", "Nainen", "06.08.1995", "törkeä huumausainerikos", 1, "Maiju Mäkelä"));
+        
 
+    }
+    
+    public ArrayList<Vanki> vangitLista(){
+        return vangitLista;
     }
 
     public void lisaaVanki(String vankinimi, String sukupuoli, String syntymapv, String rikos, int tuomioaika, String vanginhoitaja) {
