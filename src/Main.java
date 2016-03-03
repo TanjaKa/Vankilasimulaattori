d2016ce10e4282f833dded65c1d328eac5c1e6d7
tiina <tiina@DESKTOP-D2K8O0N.wlan.metropolia.fi>
2016-03-03 09:55:04 +0200

    LISÄTTY MAIN ERIKSEEN. Tehty paljon muutoksia ja STATIC ONGELMA ON POISSA JEA

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+   public static View view = new View();
+
+
+    
+
+    public static void main(String[] args) {
+        
+//        view.alkuValikko();
+
+        String menu = "1: Vangit\n"
+                + "2: Paikka\n"
+                + "3: Käytös\n"
+                + "4: Vankienhoitajat\n"
+                + "5: Tuomiot\n"
+                + "9: LOPETA\n"
+                + "Anna valintasi: ";
+
+        int valinta = 0;
+        String lukuStr;
+
+        do {
+            lukuStr = JOptionPane.showInputDialog(menu);
+            valinta = Integer.parseInt(lukuStr);
+            switch (valinta) {
+                case 1:
+                    view.vangitNakyma();
+                    break;
+                case 2:
+                    view.paikkaNakyma();
+                    break;
+                case 3:
+                    view.kaytosNakyma();
+                    break;
+                case 4:
+                    view.vankienhoitajatNakyma();
+                    break;
+                case 5:
+                    view.tuomiotNakyma();
+                case 9:
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Virheellinen valinta");
+            }
+        } while (valinta != 9);
+    }
+
+}
