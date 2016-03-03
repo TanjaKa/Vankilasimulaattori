21a2201de710a6b45a0eadc88feec47583679c8a
tiina <tiina@DESKTOP-D2K8O0N.wlan.metropolia.fi>
2016-03-03 09:55:21 +0200

    Tehty paljon muutoksia ja STATIC ONGELMA ON POISSA JEA

--- src/View.java
+++ src/View.java
@@ -4,17 +4,53 @@ Täältä kutsutaan kontrolleria ja annetaan käyttöliittymä (JOptionPane)
 import javax.swing.JOptionPane;
 
 public class View {
+       public static Kontrolleri kontrolleri = new Kontrolleri();
 
-    private View view;
-
-    private Kontrolleri kontrolleri;
 
     private String vankiString;
+   
 
-    public View(View view, Kontrolleri kontrolleri) {
-        this.view = view;
-        this.kontrolleri = kontrolleri;
+    public View() {
+        
 
+    }
+    
+    public void alkuValikko(){
+         String menu = "1: Vangit\n"
+                + "2: Paikka\n"
+                + "3: Käytös\n"
+                + "4: Vankienhoitajat\n"
+                + "5: Tuomiot\n"
+                + "9: LOPETA\n"
+                + "Anna valintasi: ";
+
+        int valinta = 0;
+        String lukuStr;
+
+        do {
+            lukuStr = JOptionPane.showInputDialog(menu);
+            valinta = Integer.parseInt(lukuStr);
+            switch (valinta) {
+                case 1:
+                    vangitNakyma();
+                    break;
+                case 2:
+                    paikkaNakyma();
+                    break;
+                case 3:
+                    kaytosNakyma();
+                    break;
+                case 4:
+                    vankienhoitajatNakyma();
+                    break;
+                case 5:
+                    tuomiotNakyma();
+                case 9:
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Virheellinen valinta");
+            }
+        } while (valinta != 9);
     }
 
     public String getVankiString() {
@@ -138,44 +174,7 @@ public class View {
          */
     }
 
-    public static void main(String[] args) {
-
-        String menu = "1: Vangit\n"
-                + "2: Paikka\n"
-                + "3: Käytös\n"
-                + "4: Vankienhoitajat\n"
-                + "5: Tuomiot\n"
-                + "9: LOPETA\n"
-                + "Anna valintasi: ";
-
-        int valinta = 0;
-        String lukuStr;
-
-        do {
-            lukuStr = JOptionPane.showInputDialog(menu);
-            valinta = Integer.parseInt(lukuStr);
-            switch (valinta) {
-                case 1:
-                    vangitNakyma();
-                    break;
-                case 2:
-                    paikkaNakyma();
-                    break;
-                case 3:
-                    kaytosNakyma();
-                    break;
-                case 4:
-                    vankienhoitajatNakyma();
-                    break;
-                case 5:
-                    tuomiotNakyma();
-                case 9:
-                    break;
-                default:
-                    JOptionPane.showMessageDialog(null, "Virheellinen valinta");
-            }
-        } while (valinta != 9);
-    }
+   
 
 // ArrayList<Paikka> vankilat = new ArrayList();
     // vankilat.add(new Paikka(avovankila));
