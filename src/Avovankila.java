0b8a9bd8a25892a947adfc6a541584c6ee107199
Omistaja <Omistaja@10.112.195.122>
2016-03-07 11:05:28 +0200

    luotu uusi luokka

---
+++ src/Avovankila.java
@@ -0,0 +1,47 @@
+
+import java.util.ArrayList;
+
+public class Avovankila {
+
+    private ArrayList<Vanki> vangitLista;
+
+    private Vanki vanki;
+
+    public Avovankila() {
+
+    }
+
+    public ArrayList<Vanki> getVangitLista() {
+        return vangitLista;
+    }
+
+    public void lisaaVanki(Vanki vanki) {
+        vangitLista.add(vanki);
+    }
+
+    public boolean poistaVanki(int vankinro) {
+
+        for (Vanki v : vangitLista) {
+            if (vankinro == v.getVankinro()) {
+                vangitLista.remove(v);
+                return true;
+            } else {
+                return false;
+
+            }
+
+        }
+
+        return false;
+
+    }
+    public String VankilanVangittoString() {
+        String vangitStr = "";
+
+        for (Vanki v : vangitLista) {
+            vangitStr = vangitStr + v.toString();
+
+        }
+        return vangitStr;
+    }
+}
