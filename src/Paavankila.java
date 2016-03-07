9c8b481f83e4a666e087e9e22f3be29ab9856f1d
Omistaja <Omistaja@10.112.195.122>
2016-03-07 11:05:03 +0200

    luotu uusi luokka

---
+++ src/Paavankila.java
@@ -0,0 +1,48 @@
+
+import java.util.ArrayList;
+
+public class Paavankila {
+
+    private ArrayList<Vanki> vangitLista;
+
+    private Vanki vanki;
+
+    public Paavankila() {
+
+    }
+
+    public ArrayList<Vanki> getVangitLista() {
+        return vangitLista;
+    }
+
+    public void lisaaVanki(Vanki vanki) {
+        vangitLista.add(vanki);
+    }
+
+    public boolean poistaVanki(int vankinro) {
+
+        for (Vanki v : vangitLista) {
+            if (vankinro == v.getVankinro()) {
+                vangitLista.remove(v);
+                return true;
+            } else {
+                return false;
+
+            }
+
+        }
+
+        return false;
+
+    }
+
+    public String VankilanVangittoString() {
+        String vangitStr = "";
+
+        for (Vanki v : vangitLista) {
+            vangitStr = vangitStr + v.toString();
+
+        }
+        return vangitStr;
+    }
+}
