6daae41339c2965ef80f4f55f33f541cfe926678
Omistaja <Omistaja@10.112.195.122>
2016-03-07 11:04:47 +0200

    luotu uusi luokka

---
+++ src/Naisvankila.java
@@ -0,0 +1,48 @@
+
+import java.util.ArrayList;
+
+public class Naisvankila {
+
+    private ArrayList<Vanki> vangitLista;
+
+    private Vanki vanki;
+
+    public Naisvankila() {
+
+    }
+
+    public ArrayList<Vanki> getVangitLista() {
+        return vangitLista;
+    }
+
+    public void lisaaVanki(Vanki vanki) {
+        vangitLista.add(vanki);
+    }
+
+    public boolean poistaVanki(int vankinro) {
+
+        for (Vanki v : vangitLista) {
+            if (vankinro == v.getVankinro()) {
+                vangitLista.remove(v);
+                return true;
+            } else {
+                return false;
+
+            }
+
+        }
+
+        return false;
+
+    }
+
+    public String VankilanVangittoString() {
+        String vangitStr = "";
+
+        for (Vanki v : vangitLista) {
+            vangitStr = vangitStr + v.toString();
+
+        }
+        return vangitStr;
+    }
+}
