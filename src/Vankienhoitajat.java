78df62b6fc4cf0b15c41c48656ab586fc6ff876e
Omistaja <Omistaja@10.112.195.122>
2016-03-07 12:41:20 +0200

    jotain tehty

--- src/Vankienhoitajat.java
+++ src/Vankienhoitajat.java
@@ -1,14 +1,29 @@
 /*
 tänne vankienhoitajatiedot
-*/
-public class Vankienhoitajat {
-    
-   String vankienhoitaja;
-   String vastuuvangit;
+ */
+import java.util.ArrayList;
 
-    public Vankienhoitajat(String vankienhoitaja, String vastuuvangit) {
-        this.vankienhoitaja = vankienhoitaja;
-        this.vastuuvangit = vastuuvangit;
+public class Vankienhoitajat {
+
+    private ArrayList<Vankienhoitaja> vankienhoitajaLista;
+    private Vankienhoitaja vankienhoitaja;
+
+    public Vankienhoitajat() {
+
+        vankienhoitajaLista = new ArrayList<Vankienhoitaja>();
+
+        vankienhoitajaLista.add(new Vankienhoitaja(1, "Teemu Kontiolahti"));
+        vankienhoitajaLista.add(new Vankienhoitaja(2, "Lauri Ronkainen"));
+        vankienhoitajaLista.add(new Vankienhoitaja(3, "Maiju Mäkelä"));
+
     }
-    
+    public String VankienhoitajatToString() {
+        String vankienhoitajatStr = "";
+
+        for (Vankienhoitaja v : vankienhoitajaLista) {
+            vankienhoitajatStr = vankienhoitajatStr + v.toString() +"\n";
+
+        }
+        return vankienhoitajatStr;
+    }
 }
